375153bae6bacf68c91a9749e3ee8a512cd5e501
Henyelrey <henyel91@gmail.com>
2024-08-26 07:57:13 -0500

    first commit

---
+++ ms-catalogo/src/main/java/com/example/mscatalogo/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.mscatalogo.repository;
+
+import com.example.mscatalogo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
